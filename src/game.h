0a7c3bfe2e7877376226f629379acffbb79f0b94
Henrique Santana <henriquessantana314@gmail.com>
2023-09-04 11:30:30 -0300

    Remoção de enum não usado

--- src/game.h
+++ src/game.h
@@ -13,16 +13,6 @@ typedef enum {
     MODE_BATCH = 2
 } GameMode;
 
-typedef enum {
-    ACTION_EXIT = 0,                     // X
-    ACTION_DRAW = 1,                     // CC
-    ACTION_WASTE_TO_FOUNDATION = 2,      // DB
-    ACTION_WASTE_TO_TABLEAU = 3,         // DT
-    ACTION_TABLEAU_TO_FOUNDATION = 4,    // TB
-    ACTION_FOUNDATION_TO_TABLEAU = 5,    // BT
-    ACTION_BETWEEN_TABLEAU = 6           // TT
-} GameAction;
-
 void Game__run(SolitaireTable *slt);
 GameMode Game__select_mode(FILE **f);
 void Game__init_interactive(SolitaireTable *slt);
